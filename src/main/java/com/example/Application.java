7ffc1021f7b28cd8f8543f93e26ca96df1b550c0
Paul Nogas <Paul.Nogas@soti.net>
2020-07-10 16:30:52 -0400

    anotherTest

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -3,5 +3,8 @@ package com.example;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        for (int i = 0 ; i < 7; i++){
+            System.out.println("Hello world " + i);
+        }
     }
 }
